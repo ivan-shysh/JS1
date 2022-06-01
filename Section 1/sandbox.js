2a10755375fce57bc01d47de9484a0599faecc0b
ivansh83 <ivansh83@gmail.com>
2022-06-01 01:32:39 -0400

    first commit

---
+++ Section 1/sandbox.js
@@ -0,0 +1,15 @@
+//Пример 1. Напечатать в консоли название вашего города.
+console.log('Miami');
+//Пример 2. Напечатать свое имя, затем закомментировать эту строку.
+//console.log('Ivan');
+//Пример 3. Напечатать несколько строк стихотворения.
+// 1 cпособ:
+console.log('Let it snow!');
+console.log('Let it snow!');
+console.log('Let it snow!');
+// 2 способ:
+console.log('Let it snow!\nLet it snow!\nLet it snow!'); 
+
+//Coding Practice
+//Cоздайте переменную x и присвойте ей значение 45
+const x = 45;
